4be2fb8c72b18a761a361558d6b9f7f83dbf341f
Guy Marshall <guymarshall9@gmail.com>
2022-08-28 10:17:28 +0100

    Add random colour for sliderValue

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
     console.log(output.innerHTML);
+    document.getElementById("sliderValue").style.color = getRandomColor();
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
